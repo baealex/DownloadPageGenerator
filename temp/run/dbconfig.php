5f2e3c6f944ce142a6ed5423f8527bd93833c90a
Jino Bae <bae_alex@naver.com>
2019-05-02 20:51:56 +0900

    [feat] remove php file

--- temp/run/dbconfig.php
+++
@@ -1,8 +0,0 @@
-<?php
-$dbconfig_host = "localhost";
-$dbconfig_name = "subscribe-db";
-$dbconfig_user = "-";
-$dbconfig_passwd = "-";
-
-$pdo = new PDO('mysql:host='.$dbconfig_host.';dbname='.$dbconfig_name.';', $dbconfig_user, $dbconfig_passwd);
-?>
--- temp/run/initdb.php
+++
@@ -1,5 +0,0 @@
-<?php include 'dbconfig.php'?>
-<?php
-// CREATE TABLE
-$statement = $pdo->query("CREATE TABLE USERINFO(EMAIL char(30), PASSWD char(255))");
-?>
