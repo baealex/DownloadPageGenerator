0986bec82a633fb81a8486874dd48001c6ce573b
Jino Bae <bae_alex@naver.com>
2019-05-02 20:50:14 +0900

    [feat] make signup page

--- src/subscribe/views.py
+++ src/subscribe/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def main_page(request):
+    return render(request, 'main_page.html',{ })
+
+def signup_page(request):
+    return render(request, 'signup_page.html', { })
