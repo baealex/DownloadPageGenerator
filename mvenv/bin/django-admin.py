ab7b6d4e9ffc1d58ee32e1f509a6838a3460bbf4
baealex <im@baejino.com>
2019-05-12 11:22:56 +0900

    [del] mvenv

--- mvenv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/srv/dev-disk-by-id-usb-JMicron_Tech_DB9876543211163-0-0-part1/WebServer/GitHub/BlogSubscribeService/mvenv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
