23cf7c11e7e8abf98ddadcb91fa50dcd10b2d103
Jino Bae <im@baejino.com>
2019-10-30 16:55:21 +0900

    [delete] clear repo

--- src/subscribe/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('',views.login_page,name='login_page'),
-    path('signup',views.signup_page,name='signup_page'),
-    path('home',views.home_page,name='home_page'),
-    path('setting',views.setting_page,name='setting_page'),
-]
